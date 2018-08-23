dd1921d2731c872414de0ce9e0829f6a5b48284b
Matt Keen <matt@mgkeen.com>
2018-08-23 15:33:22 +1200

    C# project backbone

---
+++ src/MagicWeaponShop.Tests/TruthTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MagicWeaponShop.Tests
+{
+    public class TruthTests
+    {
+
+        [Fact]
+        public void Truth_is_truth()
+        {
+            Assert.True(true);
+        }
+    }
+}
