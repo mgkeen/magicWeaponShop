f0edd00c526d32c6a36fd032de9ec7e3acfb5c25
Matt Keen <matt@mgkeen.com>
2018-08-23 16:04:04 +1200

    Task 2

--- src/MagicWeaponShop.Tests/SwordTests.cs
+++ src/MagicWeaponShop.Tests/SwordTests.cs
@@ -12,22 +12,22 @@ namespace MagicWeaponShop.Tests
         [Fact]
         public void New_swords_start_with_50_magic()
         {
-            var sword = new Sword();
+            var sword = Sword.NewSword();
             Assert.Equal(50, sword.MagicAmount);
         }
 
         [Fact]
         public void After_one_day_a_sword_loses_5_magic()
         {
-            var sword = new Sword();
+            var sword = Sword.NewSword();
             sword.MidnightStrikes();
             Assert.Equal(45, sword.MagicAmount);
         }
 
         [Fact]
-        public void A_sword_is_worth_twice_its_magic_level_in_gold()
+        public void A_sword_is_worth_twice_its_magic_amount_in_gold()
         {
-            var sword = new Sword();
+            var sword = Sword.NewSword();
 
             Assert.Equal(50, sword.MagicAmount);
             Assert.Equal(100, sword.Price);
@@ -36,6 +36,31 @@ namespace MagicWeaponShop.Tests
 
             Assert.Equal(45, sword.MagicAmount);
             Assert.Equal(90, sword.Price);
+        }
+
+        [Fact]
+        public void Second_hand_swords_start_with_less_magic()
+        {
+            var sword = Sword.SecondHandSword();
+            
+            Assert.Equal(25, sword.MagicAmount);
+        }
+
+        [Fact]
+        public void Second_hand_swords_lose_5_magic_per_day()
+        {
+            var sword = Sword.SecondHandSword();
+            sword.MidnightStrikes();
+            
+            Assert.Equal(20, sword.MagicAmount);
+        }
+
+        [Fact]
+        public void Second_hand_swords_sell_for_their_magic_amount_in_gold()
+        {
+            var sword = Sword.SecondHandSword();
+
+            Assert.Equal(25, sword.Price);
         }
     }
 }
--- src/MagicWeaponShop/Sword.cs
+++ src/MagicWeaponShop/Sword.cs
@@ -8,17 +8,30 @@ namespace MagicWeaponShop
 {
     public class Sword
     {
-        public Sword()
+        private readonly int _priceMultiplier;
+
+        internal Sword(int startingMagicAmount, int priceMultiplier)
         {
-            MagicAmount = 50;
+            _priceMultiplier = priceMultiplier;
+            MagicAmount = startingMagicAmount;
         }
 
         public int MagicAmount { get; private set; }
-        public int Price => MagicAmount * 2;
+        public int Price => MagicAmount * _priceMultiplier;
 
         public void MidnightStrikes()
         {
             MagicAmount = MagicAmount - 5;
+        }
+
+        public static Sword NewSword()
+        {
+            return new Sword(50, 2);
+        }
+
+        public static Sword SecondHandSword()
+        {
+            return new Sword(25, 1);
         }
     }
 }
