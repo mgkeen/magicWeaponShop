e1af48eb21de6f050ca2c137dc45371791e875b4
Aron Adler <aronadler@hotmail.com>
2018-09-06 17:01:07 +0100

    Tasks 1 and 2

---
+++ src/MagicWeaponShop.Tests/SwordTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MagicWeaponShop.Tests
+{
+    public class SwordTests
+    {
+
+        [Fact]
+        public void NewSwordHas50Magic()
+        {
+            var sword = new Sword();
+            Assert.Equal(Sword.InitialMagicSize, sword.Magic);
+        }
+
+        [Fact]
+        public void StrikeMidnightLowersMagicBy5()
+        {
+            var sword = new Sword();
+            sword.StrikeMidnight();
+            Assert.Equal(Sword.InitialMagicSize - 5, sword.Magic);
+        }
+
+        [Fact]
+        public void SellingPriceForNewSwordIsDoubleMagic()
+        {
+            var sword = new Sword();
+            Assert.Equal(Sword.InitialMagicSize * 2, sword.SellingPrice);
+        }
+
+        [Fact]
+        public void SellingPriceForNewSwordIsMagic()
+        {
+            var sword = GetSecondHandSword();
+            Assert.Equal(Sword.InitialMagicSize / 2, sword.SellingPrice);
+        }
+
+        [Fact]
+        public void SecondHandSwordHas50Magic()
+        {
+            var sword = GetSecondHandSword();
+            Assert.Equal(Sword.InitialMagicSize / 2, sword.Magic);
+        }
+
+        private static Sword GetSecondHandSword()
+        {
+            var sword = new Sword();
+            sword.MakeSecondHand();
+            return sword;
+        }
+    }
+}
---
+++ src/MagicWeaponShop/Sword.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicWeaponShop
+{
+    public class Sword : ISword
+    {
+        public static int InitialMagicSize = 50;
+        public bool IsSecondHand;
+        private int _multiplier;
+
+        public Sword()
+        {
+            _multiplier = 2;
+            Magic = InitialMagicSize;
+            IsSecondHand = false;
+        }
+
+        public int Magic { get; private set; }
+
+        public void StrikeMidnight()
+        {
+            Magic = Magic - 5;
+        }
+
+        public void MakeSecondHand()
+        {
+            IsSecondHand = true;
+            Magic = InitialMagicSize / 2;
+            _multiplier = 1;
+        }
+
+        public int SellingPrice => Magic * _multiplier;
+    }
+}
