a11519bd189d1c5ee33b39107af025aa83e49bc2
iagomartinez <iago@fundapps.co>
2018-09-06 17:25:01 +0100

    New and Second hand swords

--- src/MagicWeaponShop.Tests/TruthTests.cs
+++ src/MagicWeaponShop.Tests/TruthTests.cs
@@ -9,11 +9,53 @@ namespace MagicWeaponShop.Tests
 {
     public class TruthTests
     {
+        [Fact]
+        public void New_Magic_Swords_Have_50_Magic()
+        {
+            var newSword = new NewSword();
+            Assert.Equal(50, newSword.Magic);
+        }
 
         [Fact]
-        public void Truth_is_truth()
+        public void When_Clock_Says_Midnight_Magic_Decreases()
         {
-            Assert.True(true);
+            var shop = new Shop(new List<MagicSword> {new NewSword(), new SecondHandSword()});
+
+            shop.ItsMidnight();
+
+            foreach (var s in shop.Swords)
+            {
+                if (s is NewSword)
+                    Assert.Equal(45, s.Magic);
+                else if (s is SecondHandSword)
+                    Assert.Equal(20, s.Magic);
+
+            }
+        }
+
+        [Fact]
+        public void NewMagicSword_Value_Is_Doubled_Magic()
+        {
+            var shop = new Shop(new List<MagicSword> { new NewSword() });
+
+            foreach (var s in shop.Swords)
+                Assert.Equal(100, s.Price);
+        }
+
+        [Fact]
+        public void SeconHand_Magic_Swords_Have_25_Magic()
+        {
+            var sword = new SecondHandSword();
+            Assert.Equal(25, sword.Magic);
+        }
+
+        [Fact]
+        public void SecondHandMagicSword_Value_Is_Doubled_Magic()
+        {
+            var shop = new Shop(new List<MagicSword> { new SecondHandSword() });
+
+            foreach (var s in shop.Swords)
+                Assert.Equal(25, s.Price);
         }
     }
 }
---
+++ src/MagicWeaponShop/MagicSword.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MagicWeaponShop
+{
+    public class Shop
+    {
+        public Shop(List<MagicSword> swords)
+        {
+            Swords = swords;
+        }
+
+        public List<MagicSword> Swords { get; }
+
+        public void ItsMidnight()
+        {
+            foreach (var s in Swords)
+                s.Degrade();
+
+        }
+    }
+
+    public abstract class MagicSword
+    {
+        private readonly Func<int, int> _priceCalculator;
+
+        protected MagicSword(int initialMagic, Func<int, int> priceCalculator)
+        {
+            Magic = initialMagic;
+            _priceCalculator = priceCalculator;
+        }
+
+        public int Magic { get; private set; }
+        public int Price => _priceCalculator(Magic);
+
+        public void Degrade()
+        {
+            Magic -= 5;
+        }
+    }
+
+    public class NewSword : MagicSword
+    {
+        public NewSword() : base(50, x => x * 2)
+        {
+            
+        }
+    }
+
+    public class SecondHandSword : MagicSword
+    {
+        public SecondHandSword() : base(25, x => x)
+        {
+
+        }
+    }
+
+}
